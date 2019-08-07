a76a15e8098312b2dec033f213be3432d8c7f7cd
Macmini <wangdongya1990@163.com>
2019-08-07 16:59:52 +0800

    update

--- UIViewChainCode/ViewController.m
+++ UIViewChainCode/ViewController.m
@@ -18,6 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    //创建UI
     [self setUI];
     // Do any additional setup after loading the view.
 }
